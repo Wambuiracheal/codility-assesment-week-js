12f7eed4ffbc69689483d00fcb7927912f605b09
Racheal <rachealnjugush@gmail.com>
2025-02-13 07:24:19 +0300

    codility

--- dept.js
+++
@@ -1,8 +0,0 @@
-function solution(A){
-    let counts = A.reduce((map, dept) => (map[dept] = (map[dept] || 0) + 1, map), {});
-    return Math.max(...Object.values(counts))
-  }
-
-console.log(solution(["Cardiology", "Orthopaedics", "Neurology", "Cardiology", "Orthopaedics", "Cardiology"]))
-console.log(solution( ["Oncology", "Gynaecology", "Orthopaedics", "Oncology", "Gynaecology", "Orthopaedics"]))
-console.log(solution( ["Neurology", "Cardiology", "Oncology"]))
